8b95a406bcd384016ed30d26d2ab6c9ed27e9a67
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-05-31 14:12:52 -0300

    Adiciona migrations para criação das tabelas no banco e as factories dummy data

---
+++ exercicio08/database/factories/ArtigoFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Artigo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ exercicio08/database/migrations/2018_05_24_031323_create_artigos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtigosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artigos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->text('conteudo');
+            $table->integer('autor_id')->unsigned();
+            $table->integer('categoria_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('autor_id')->references('id')->on('autores');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artigos');
+    }
+}
